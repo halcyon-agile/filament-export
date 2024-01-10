c4d7edb4b1ec5f6185d3fe2ad84aaa5c388317c8
Lloric Mayuga Garcia <lloricode@gmail.com>
2024-01-10 11:32:36 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- config/filament-export.php
+++ config/filament-export.php
@@ -11,13 +11,4 @@ return [
     'user_timezone_field' => 'timezone',
 
     'expires_in_minute' => 60,
-
-//    'http' => [
-//        'route' => [
-//            'name' => 'filament-export.download',
-//            'path' => 'admin/export/download',
-//            'middleware' => []
-//        ]
-//    ]
-
 ];
--- src/FilamentExportServiceProvider.php
+++ src/FilamentExportServiceProvider.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace HalcyonAgile\FilamentExport;
 
 use HalcyonAgile\FilamentExport\Commands\PruneExportCommand;
-use HalcyonAgile\FilamentExport\Http\Controllers\DownloadExportController;
-use Illuminate\Routing\Middleware\ValidateSignature;
-use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -20,16 +17,4 @@ class FilamentExportServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasCommand(PruneExportCommand::class);
     }
-
-    //    public function packageBooted(): void
-    //    {
-    //        Route::get(
-    //            config('filament-export.http.route.path').'/{fileName}',
-    //            DownloadExportController::class
-    //        )
-    //            ->where('fileName', '.*')
-    ////            ->middleware(ValidateSignature::relative())
-    //            ->middleware(config('filament-export.http.route.middleware'))
-    //            ->name(config('filament-export.http.route.name'));
-    //    }
 }
